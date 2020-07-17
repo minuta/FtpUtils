0d21149a93bead12d2060a173e7ee5bc281ed721
AF <af@kfw.de>
2020-07-17 16:29:03 +0200

    refactor the FTP server class

--- src/main/java/FTP/FtpServer.java
+++ src/main/java/FTP/FtpServer.java
@@ -12,23 +12,30 @@ import org.apache.ftpserver.usermanager.impl.BaseUser;
  */
 public class FtpServer {
 
+    public static final String USER = "dummyUser";
+    public static final String PASS = "dummyPass";
+    public static final String HOME = "/tmp";
+    public static final int PORT = 2221;
+    public static final String DEFAULT_LISTENER = "default";
+
     private org.apache.ftpserver.FtpServer server;
 
     public void start() throws FtpException {
         PropertiesUserManagerFactory userManagerFactory = new PropertiesUserManagerFactory();
+
         UserManager userManager = userManagerFactory.createUserManager();
         BaseUser user = new BaseUser();
-        user.setName("dummyUser");
-        user.setPassword("dummyPass");
-        user.setHomeDirectory("/tmp");
+        user.setName(USER);
+        user.setPassword(PASS);
+        user.setHomeDirectory(HOME);
         userManager.save(user);
 
         ListenerFactory listenerFactory = new ListenerFactory();
-        listenerFactory.setPort(2221);
+        listenerFactory.setPort(PORT);
 
         FtpServerFactory factory = new FtpServerFactory();
         factory.setUserManager(userManager);
-        factory.addListener("default", listenerFactory.createListener());
+        factory.addListener(DEFAULT_LISTENER, listenerFactory.createListener());
 
         server = factory.createServer();
         server.start();
