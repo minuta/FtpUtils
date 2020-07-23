2fb2da8b37d752b16eefd27315c9046e9b23f5fa
AF <af@kfw.de>
2020-07-23 14:24:47 +0200

    add a constructor-parameter for configuring of the FTPS connection mode (explicit/implicit)

--- src/main/java/FTPS/FtpsServer.java
+++ src/main/java/FTPS/FtpsServer.java
@@ -14,21 +14,23 @@ public class FtpsServer {
     private org.apache.ftpserver.FtpServer server;
     private final int port;
     private final String keystorePassword;
+    public boolean isImplicitMode;
 
     public static final String KEYSTORE_PATH = "src/main/resources/ftpserver.jks";
     public static final String USER_PROPERTIES_PATH = "src/main/resources/user.properties";
     public static final int DEFAULT_PORT = 2221;
     public static final String DEFAULT_PASS = "password";
+    public static final String DEFAULT_LISTENER = "default";
 
-    // TODO: make a constructor with the connection-mode (implicite/explicite)
 
     public FtpsServer() {
-        this(DEFAULT_PORT, DEFAULT_PASS);
+        this(DEFAULT_PORT, DEFAULT_PASS, false);
     }
 
-    public FtpsServer(int port, String keystorePassword) {
+    public FtpsServer(int port, String keystorePassword, boolean isImplicitMode) {
         this.port = port;
         this.keystorePassword = keystorePassword;
+        this.isImplicitMode = isImplicitMode;
     }
 
 
@@ -49,7 +51,7 @@ public class FtpsServer {
         factory.setImplicitSsl(false);
 
         // replace the default listener
-        serverFactory.addListener("default", factory.createListener());
+        serverFactory.addListener(DEFAULT_LISTENER, factory.createListener());
         PropertiesUserManagerFactory userManagerFactory = new PropertiesUserManagerFactory();
         userManagerFactory.setFile(new File(USER_PROPERTIES_PATH));
         serverFactory.setUserManager(userManagerFactory.createUserManager());
