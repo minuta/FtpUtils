3b6e09149a11e6dda2754740fbae183dac97fd1f
AF <af@kfw.de>
2020-08-07 16:49:15 +0200

    add constructors for handling users via user.properties file and a hardcoded ftp-user

--- src/main/java/FTPS/FtpsServer.java
+++ src/main/java/FTPS/FtpsServer.java
@@ -20,15 +20,13 @@ public class FtpsServer {
     private org.apache.ftpserver.FtpServer server;
     private final int port;
     private final String keystorePassword;
-//    private final String ftpHomeDir;
     private final String keyStorePath;
-//    private final String userPropertiesPath;
+    private String userPropertiesPath = null;
     public boolean isImplicitMode;
 
-    private final String user;
-    private final String userPass;
-    private final String userHomeDir;
-//    private final int port;
+    private String user = null;
+    private String userPass = null;
+    private String userHomeDir = null;
 
     public static final String KEYSTORE_PATH = "FTPS_SERVER_SETTINGS/ftpserver.jks";
     public static final String USER_PROPERTIES_PATH = "FTPS_SERVER_SETTINGS/user.properties";
@@ -36,19 +34,29 @@ public class FtpsServer {
     public static final String DEFAULT_PASS = "password";
     public static final String DEFAULT_LISTENER = "default";
 
-//    public FtpsServer() {
-//        this(DEFAULT_PORT, DEFAULT_PASS, false);
-//    }
 
+    /**
+     *   constructor for creating an FTPS server with a single user
+     */
     public FtpsServer(int port, String keyStorePath, String keystorePassword, String user, String userPass, String userHomeDir, boolean isImplicitMode) {
         this.port = port;
         this.keystorePassword = keystorePassword;
         this.isImplicitMode = isImplicitMode;
         this.keyStorePath = keyStorePath;
-//        this.userPropertiesPath = userPropertiesPath;
         this.user = user;
         this.userPass = userPass;
         this.userHomeDir = userHomeDir;
+    }
+
+    /**
+     *   constructor for creating an FTPS server via the file user.properties
+     */
+    public FtpsServer(int port, String keyStorePath, String keystorePassword, String userPropertiesPath, boolean isImplicitMode) {
+        this.port = port;
+        this.keystorePassword = keystorePassword;
+        this.isImplicitMode = isImplicitMode;
+        this.keyStorePath = keyStorePath;
+        this.userPropertiesPath = userPropertiesPath;
     }
 
     public void createUser(UserManager userManager, String userName, String userPass, String userHomeDir) throws FtpException {
@@ -60,9 +68,46 @@ public class FtpsServer {
         userManager.save(user);
     }
 
+    public void startWithUserPropertiesFile() throws FtpException {
+        FtpServerFactory serverFactory = new FtpServerFactory();
+        ListenerFactory factory = new ListenerFactory();
+
+        // set the port of the listener
+        factory.setPort(port);
+
+        // define SSL configuration
+        SslConfigurationFactory ssl = new SslConfigurationFactory();
+        ssl.setKeystoreFile(new File(keyStorePath));
+        ssl.setKeystorePassword(keystorePassword);
+
+        // set the SSL configuration for the listener
+        factory.setSslConfiguration(ssl.createSslConfiguration());
+        factory.setImplicitSsl(false); // TODO: should be not hardcoded!
+
+        // replace the default listener
+        serverFactory.addListener(DEFAULT_LISTENER, factory.createListener());
+
+//        PropertiesUserManagerFactory userManagerFactory = new PropertiesUserManagerFactory();
+//        UserManager userManager = userManagerFactory.createUserManager();
+//        createUser(userManager, user, userPass, userHomeDir);
 
 
-    public void start() throws FtpException {
+        PropertiesUserManagerFactory userManagerFactory = new PropertiesUserManagerFactory();
+        userManagerFactory.setFile(new File(userPropertiesPath));
+        serverFactory.setUserManager(userManagerFactory.createUserManager());
+
+//        FtpServerFactory factory = new FtpServerFactory();
+//        serverFactory.setUserManager(userManager);
+
+//        server = factory.createServer();
+//        server.start();
+
+        // start the server
+        FtpServer server = serverFactory.createServer();
+        server.start();
+    }
+
+    public void startWithSingleUser() throws FtpException {
         FtpServerFactory serverFactory = new FtpServerFactory();
         ListenerFactory factory = new ListenerFactory();
 
