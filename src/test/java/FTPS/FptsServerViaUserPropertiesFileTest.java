34e3621532223bc69c71fd3e6884a0bad0dc99ca
AF <af@kfw.de>
2020-08-07 16:50:09 +0200

    add a new test for testing FTPS server and FTPS client based on a user.properties file

---
+++ src/test/java/FTPS/FptsServerViaUserPropertiesFileTest.java
@@ -0,0 +1,72 @@
+package FTPS;
+
+import org.apache.ftpserver.ftplet.FtpException;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
+public class FptsServerViaUserPropertiesFileTest {
+    public static final String USER = "admin";
+    public static final String PASS = "admin";
+    public static int PORT = 2221;
+
+    static private FtpsServer ftpsServer;
+    static private FtpsClient ftpsClient;
+
+    public static final String DIR_FILE = ".keep";
+    public static final String ROOT_FILE1 = "file1.txt";
+    public static final String ROOT_FILE2 = "file2.txt";
+
+    public static final String FTP_RESOURCES_PATH = new File("src/test/resources/FTP_HOME").getPath();  // for a Linux/Win compatibility
+    public static final String FTPS_SERVER_SETTINGS = new File("src/test/resources/FTPS_SERVER_SETTINGS").getPath();
+
+    @BeforeClass
+    public static void setup() throws FtpException {
+        ftpsServer = new FtpsServer(2221,
+                FTPS_SERVER_SETTINGS + File.separator + "ftpserver.jks",
+                FtpsServer.DEFAULT_PASS,
+                FTPS_SERVER_SETTINGS + File.separator + "user.properties",
+                false);
+        ftpsServer.startWithUserPropertiesFile();
+
+    }
+
+//    @After
+//    public void teardown() throws IOException {
+//        ftpsClient.close();
+//        ftpsServer.stop();           // this method throws a NullPointerException
+//    }
+
+
+    @Test
+    public void dummy() {
+    }
+
+    @Test
+    public void connectionTest() throws IOException {
+        ftpsClient = new FtpsClient("localhost", PORT, USER, PASS);
+        ftpsClient.open();
+        ftpsClient.close();
+    }
+
+    @Test
+    public void listFilenamesTest() throws IOException {
+        ftpsClient = new FtpsClient("localhost", PORT, USER, PASS);
+        ftpsClient.open();
+
+        List<String> filenameList = ftpsClient.listFilenames("");
+        Assert.assertTrue(filenameList.contains(ROOT_FILE1));
+//        Assert.assertTrue(filenameList.contains(ROOT_FILE2));
+//        Assert.assertFalse(filenameList.contains(DIR_FILE));
+//
+//        filenameList = ftpsClient.listFilenames("DIR1");
+//        Assert.assertTrue(filenameList.contains(DIR_FILE));
+
+        ftpsClient.close();
+//        ftpsServer.stop();
+    }
+}
