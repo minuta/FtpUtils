ba183afab28f96afeec1d2d1404a1b27b44c61f9
AF <af@kfw.de>
2020-08-20 12:26:05 +0200

    add FtpsClient as an extension of a FtpClient

---
+++ src/main/java/client/FtpsClient.java
@@ -0,0 +1,41 @@
+package client;
+
+import org.apache.commons.net.PrintCommandListener;
+import org.apache.commons.net.ftp.FTPReply;
+import org.apache.commons.net.ftp.FTPSClient;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class FtpsClient extends FtpClient {
+
+    private final String server;
+    private final int port;
+    private final String user;
+    private final String password;
+    private FTPSClient ftp;
+
+    public FtpsClient(String server, int port, String user, String password) {
+        super(server, port, user, password);
+        this.port = port;
+        this.user = user;
+        this.password = password;
+        this.server = null;
+    }
+
+    public void open() throws IOException {
+        ftp = new FTPSClient();
+
+        ftp.addProtocolCommandListener(new PrintCommandListener(new PrintWriter(System.out)));
+
+        ftp.connect(server, port);
+        int reply = ftp.getReplyCode();
+        if (!FTPReply.isPositiveCompletion(reply)) {
+            ftp.disconnect();
+            throw new IOException("Exception in connecting to FTP Server");
+        }
+
+        ftp.login(user, password);
+    }
+
+}
